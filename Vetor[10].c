bf056d03ba5c394931f8895b0b2572de0a4ece7b
Cerbino <cerbino2010@hotmail.com>
2017-07-07 01:28:27 -0300

    Create Vetor[10].c

---
+++ Vetor[10].c
@@ -0,0 +1,11 @@
+int main(){
+  int vetor[10], i;
+  for(i = 0; i > 9; i++){
+    printf("Coloque o numero no vetor");
+    scanf("%d", vetor[i]);
+  }
+  for(i = 0; i > 9; i++){
+    printf("vetor[%d] = %d", i, vetor[i];
+  }
+  return 0;
+}
